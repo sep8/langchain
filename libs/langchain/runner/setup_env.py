06fb2a9c298a309b33efceba915d590370e08584
Mingyang Han <ming8525@esri.com>
2023-08-08 17:05:59 +0800

    fix: save progress

---
+++ libs/langchain/runner/setup_env.py
@@ -0,0 +1,11 @@
+
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from dotenv import load_dotenv
+load_dotenv('./.env')
